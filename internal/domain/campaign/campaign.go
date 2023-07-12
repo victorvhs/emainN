73f57cd15f1533cb416d3a79f616f98bc418e511
vhreis <victor.h.s.reis@gmail.com>
2023-07-12 09:37:39 -0300

    refactor dos testes

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -1,6 +1,7 @@
 package campaign
 
 import (
+	"errors"
 	"github.com/rs/xid"
 	"time"
 )
@@ -17,7 +18,16 @@ type Campaign struct {
 	Contacts         []Contact
 }
 
-func NewCampaign(name string, content string, shortDescription string, emails []string) *Campaign {
+func NewCampaign(name string, content string, shortDescription string, emails []string) (*Campaign, error) {
+
+	if name == "" {
+		return nil, errors.New("Name should not be empty")
+	} else if content == "" {
+		return nil, errors.New("Content should not be empty")
+	} else if len(emails) == 0 {
+		return nil, errors.New("Contacts should not be empty")
+	}
+
 	contacts := make([]Contact, len(emails))
 	for i, email := range emails {
 		contacts[i].Email = email
@@ -29,5 +39,5 @@ func NewCampaign(name string, content string, shortDescription string, emails []
 		ShortDescription: shortDescription,
 		CreatedOn:        time.Now(),
 		Contacts:         contacts,
-	}
+	}, nil
 }
--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -6,51 +6,67 @@ import (
 	"time"
 )
 
+var (
+	name             = "Minha campanha"
+	content          = "Conteúdo da minha campanha"
+	shortDescription = "Descrição da minha campanha"
+	contacts         = []string{"email1@a.com", "email2@a.com", "email3@a.com"}
+)
+
 func Test_NewCampaign_Create_Campaign(t *testing.T) {
-	assert := assert.New(t)
+	assertions := assert.New(t)
+	campaign, _ := NewCampaign(name, content, shortDescription, contacts)
 
-	name := "Minha campanha"
-	content := "Conteúdo da minha campanha"
-	shortDescription := "Descrição da minha campanha"
-	contacts := []string{"email1@a.com", "email2@a.com", "email3@a.com"}
-
-	campaign := NewCampaign(name, content, shortDescription, contacts)
-
-	assert.Equal(name, campaign.Name)
-	assert.Equal(content, campaign.Content)
-	assert.Equal(shortDescription, campaign.ShortDescription)
-	assert.Equal(len(contacts), len(campaign.Contacts))
+	assertions.Equal(name, campaign.Name)
+	assertions.Equal(content, campaign.Content)
+	assertions.Equal(shortDescription, campaign.ShortDescription)
+	assertions.Equal(len(contacts), len(campaign.Contacts))
 
 }
 
-func Test_NewCampaing_Not_Nil_ID(t *testing.T) {
-	assert := assert.New(t)
+func Test_NewCamping_Not_Nil_ID(t *testing.T) {
+	assertions := assert.New(t)
+	campaign, _ := NewCampaign(name, content, shortDescription, contacts)
 
-	name := "Minha campanha"
-	content := "Conteúdo da minha campanha"
-	shortDescription := "Descrição da minha campanha"
-	contacts := []string{"email1@a.com", "email2@a.com", "email3@a.com"}
-
-	campaign := NewCampaign(name, content, shortDescription, contacts)
-
-	assert.NotEmpty(campaign.ID, "ID should not be empty")
-	assert.NotNilf(campaign.ID, "ID should not be nil")
-
+	assertions.NotEmpty(campaign.ID, "ID should not be empty")
+	assertions.NotNilf(campaign.ID, "ID should not be nil")
 }
 
-func Test_NewCampaing_Not_Nil_CreatedOn(t *testing.T) {
-	assert := assert.New(t)
-
-	name := "Minha campanha"
-	content := "Conteúdo da minha campanha"
-	shortDescription := "Descrição da minha campanha"
-	contacts := []string{"email1@a.com", "email2@a.com", "email3@a.com"}
+func Test_NewCamping_Not_Nil_CreatedOn(t *testing.T) {
+	assertions := assert.New(t)
 	now := time.Now().Add(-time.Minute)
 
-	campaign := NewCampaign(name, content, shortDescription, contacts)
+	campaign, _ := NewCampaign(name, content, shortDescription, contacts)
 
-	assert.NotEmpty(campaign.CreatedOn, "CreatedOn should not be empty")
-	assert.NotNilf(campaign.CreatedOn, "CreatedOn should not be nil")
-	assert.GreaterOrEqual(campaign.CreatedOn, now, "CreatedOn should be greater or equal than now")
+	assertions.NotEmpty(campaign.CreatedOn, "CreatedOn should not be empty")
+	assertions.NotNilf(campaign.CreatedOn, "CreatedOn should not be nil")
+	assertions.GreaterOrEqual(campaign.CreatedOn, now, "CreatedOn should be greater or equal than now")
+
+}
+
+func Test_NewCamping_Not_Nil_Name(t *testing.T) {
+	assertions := assert.New(t)
+
+	_, err := NewCampaign("", content, shortDescription, contacts)
+
+	assertions.Equal("Name should not be empty", err.Error())
+
+}
+
+func Test_NewCamping_Not_Nil_Content(t *testing.T) {
+	assertions := assert.New(t)
+
+	_, err := NewCampaign(name, "", shortDescription, contacts)
+
+	assertions.Equal("Content should not be empty", err.Error())
+
+}
+
+func Test_NewCamping_Not_Nil_Contacts(t *testing.T) {
+	assertions := assert.New(t)
+
+	_, err := NewCampaign(name, content, shortDescription, []string{})
+
+	assertions.Equal("Contacts should not be empty", err.Error())
 
 }
